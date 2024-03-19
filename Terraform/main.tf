3ba9ad60d35201b55dae3a0f841b6e08e6730ad1
kinchit aggarwal <kinchitaggarwal@kinchits-MacBook-Air.local>
2024-03-20 00:37:52 +0530

    Added ebs volume

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 provider "aws" {
     alias = "secondary"
-    region = "us-west-1"
+    region = "us-east-1"
 }
 
 data "aws_ami" "dynamicAmi" {
@@ -27,19 +27,26 @@ data "aws_ami" "dynamicAmi" {
   }
 }
 
-resource "aws_key_pair" "accessKey" {
-  key_name = "access-key"
-  public_key = file("~/.ssh/access_key.pub")
+data "aws_availability_zones" "available" {
   provider = aws.secondary
 }
+
+#resource "aws_key_pair" "accessKey" {
+#  key_name = "access-key"
+#  public_key = file("~/.ssh/access_key.pub")
+#  provider = aws.secondary
+#}
 
 
 
 resource "aws_instance" "server" {
-  ami           = "${data.aws_ami.dynamicAmi.id}"
-  instance_type = "t2.micro"  
+  ami = "${data.aws_ami.dynamicAmi.id}"
+  instance_type = "t2.micro" 
+  availability_zone = data.aws_availability_zones.available.names[0] 
   security_groups = [aws_security_group.basic.name]
-  key_name = aws_key_pair.accessKey.key_name
+
+  #key_name = aws_key_pair.accessKey.key_name
+
   iam_instance_profile = "ECRAccessInstanceProfile"
   user_data = file("userdata.sh") 
   provider = aws.secondary
@@ -55,23 +62,23 @@ resource "aws_security_group" basic {
     name = "sgg"
     description = "Allow ICMP only"
     provider = aws.secondary
+    
+    #ingress {
+    #    from_port = -1
+    #    to_port = -1
+    #    protocol = "icmp"
+    #    cidr_blocks = ["0.0.0.0/0"]
+    #}
 
+    #ingress {
+    #    from_port = 22
+    #    to_port = 22
+    #    protocol = "tcp"
+    #    cidr_blocks = ["103.189.173.222/32"]
+    #}
+    
     ingress {
-        from_port = -1
-        to_port = -1
-        protocol = "icmp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["103.189.173.222/32"]
-    }
-
-    ingress {
-        from_port = 0
+        from_port = 8081
         to_port = 8081
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
@@ -84,6 +91,28 @@ resource "aws_security_group" basic {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+}
+
+resource "aws_kms_key" "awsKmsKey" {
+  provider = aws.secondary
+  description         = "KMS Key"
+  enable_key_rotation = true
+}
+
+
+resource "aws_ebs_volume" "awsEbsVolume" {
+  provider = aws.secondary
+  availability_zone = data.aws_availability_zones.available.names[0]
+  size              = 10
+  encrypted         = true
+  kms_key_id        = aws_kms_key.awsKmsKey.arn
+}
+
+resource "aws_volume_attachment" "awsVolumeAttachment" {
+  provider = aws.secondary
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.awsEbsVolume.id
+  instance_id = aws_instance.server.id
 }
 
 output "public_ip" {
